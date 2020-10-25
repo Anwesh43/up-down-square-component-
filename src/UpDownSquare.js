6fc5e6097f3153d9716e7f875ec6096d80cb3e13
Anwesh43 <anweshthecool0@gmail.com>
2020-10-25 06:24:44 +0530

    adding UpDownSquare component

---
+++ src/UpDownSquare.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import useStyle from './hooks'
+
+const UpDowSquare = ({w, h, scale, onClick}) => {
+    const {parentStyle, rectStyle} = useStyle(w, h, scale)
+    return (
+        <div style = {parentStyle()}>
+            {[0, 1].map(i => (
+                <div key = {`rect_${i}`} style = {rectStyle(i)}>
+                </div>
+              )
+            )}
+        </div>
+    )
+    
+}
+
+export default UpDowSquare
