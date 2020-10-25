6734e42d171c3070d8b288e0aec4469839db017f
Anwesh43 <anweshthecool0@gmail.com>
2020-10-25 06:19:25 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,12 @@
 import {
-  useState, useEffect
+    useState,
+    useEffect
 } from 'react'
+
+import {
+    divideScale, 
+    sinify
+} from './utils'
 
 export const useAnimatedScale = (scGap, delay) => {
     const [scale, setScale] = useState(0)
@@ -37,5 +43,42 @@ export const useDimension = () => {
     return {  
         w, 
         h
+    }
+}
+
+export const useStyle = (w, h, scale) => {
+    const size = Math.min(w, h) / 9 
+    const x = w / 2 - size / 2
+    const y = h / 2 - size / 2
+    const position = 'absolute'
+    const border = '1px solid green'
+    const background = 'green' 
+    const sf = sinify(scale)
+    return {
+        parentStyle() {
+            const width = `${size}px`
+            const height = `${size}px`
+            const top = `${y}px`
+            const left = `${x}px`
+            const WebkitTransform = `rotate(${90 * divideScale(sf, 2, 3)}deg)`
+            return {
+                position, 
+                width,
+                top,
+                left, 
+                height, 
+                border, 
+                WebkitTransform
+            }
+        },
+
+        rectStyle(i) {
+            const rh = size / 2
+            const height = `${rh}px`
+            const width = `${size * divideScale(sf, i, 3)}px`
+            const top = `${rh * i}px`
+            const left = '0px'
+            return {width, height, position, left, top, background}
+        }
     }
 }
