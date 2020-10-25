6ef7329de25a82653d63c65224f64384ac1298aa
Anwesh43 <anweshthecool0@gmail.com>
2020-10-25 06:08:47 +0530

    adding useDimension hook

--- src/hooks.js
+++ src/hooks.js
@@ -24,3 +24,18 @@ export const useAnimatedScale = (scGap, delay) => {
         }
     }
 }
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+    })
+    return {  
+        w, 
+        h
+    }
+}
